0dafd6f45d7a94d78853737c14b9517e6db8b728
hangyi <129813637+yhcss1@users.noreply.github.com>
2025-03-17 22:27:42 +0800

    build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  swcMinify: true,
   // experimental: {
   //   // Required: for next 13
   //   appDir: true
   // }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
